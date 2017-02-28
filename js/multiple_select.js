56a98576aed99acdfea917cb8680f4b00ea968ec
Ricardo Borges <ricardo@iporto.com>
2017-02-28 11:44:00 -0300

    If a options is selected, the check box will be checked

--- js/multiple_select.js
+++ js/multiple_select.js
@@ -8,12 +8,22 @@ $('document').ready(function () {
         var options = [];
 
         $("option", $(this)).each(function () {
+            var checked = "";
+            //Define Placeholder
             if($(this).hasClass('placeholder')){
                 placeholder = $(this).text();
+                return true;
             }
+
+            //Define checked
+            var selected = $(this).attr('selected');
+            if(typeof selected !== typeof undefined && selected !== false){
+                checked = "checked='checked'";
+            }
+
             var value = $(this).attr('value');
             var text = $(this).text();
-            var opt = {'textLen':text.length, 'element':$("<label><input type='checkbox' name='"+name+"' value='"+value+"'>"+text+"</label>")};
+            var opt = {'textLen':text.length, 'element':$("<label><input type='checkbox' name='"+name+"' value='"+value+"' "+checked+">"+text+"</label>")};
             options.push(opt);
         });
         $(this).after("<div class='multipleSelectOptionsWrapper'><div class='placeholder'>"+placeholder+"</div><div class='multipleSelectOptions'></div></div>");
