74f840e254d9c2b113bf4e082e63cf07ac08e1c2
Ricardo Borges <ricardo@iporto.com>
2017-03-01 10:33:46 -0300

    Refactoring multiple_select.js

--- js/multiple_select.js
+++ js/multiple_select.js
@@ -1,52 +1,52 @@
-$('document').ready(function () {
-
-    $(".multipleSelect").each(function () {
-
+(function ($) {
+    $.fn.multipleSelect = function () {
         var placeholder = "";
-        var name = $(this).attr('name');
-        var width = $(this).css('width');
+        var name = this.attr('name');
+        var width = this.css('width');
         var options = [];
 
-        $("option", $(this)).each(function () {
+        $("option", this).each(function () {
             var checked = "";
             //Define Placeholder
-            if($(this).hasClass('placeholder')){
+            if ($(this).hasClass('placeholder')) {
                 placeholder = $(this).text();
                 return true;
             }
 
             //Define checked
             var selected = $(this).attr('selected');
-            if(typeof selected !== typeof undefined && selected !== false){
+            if (typeof selected !== typeof undefined && selected !== false) {
                 checked = "checked='checked'";
             }
 
             var value = $(this).attr('value');
             var text = $(this).text();
-            var opt = {'textLen':text.length, 'element':$("<label><input type='checkbox' name='"+name+"' value='"+value+"' "+checked+">"+text+"</label>")};
+            var opt = {
+                'textLen': text.length,
+                'element': $("<label><input type='checkbox' name='" + name + "' value='" + value + "' " + checked + ">" + text + "</label>")
+            };
             options.push(opt);
         });
-        $(this).after("<div class='multipleSelectOptionsWrapper'><div class='placeholder'>"+placeholder+"</div><div class='multipleSelectOptions'></div></div>");
 
-        var m = $(this).attr('width');
-        for(var i in options){
+        this.after("<div class='multipleSelectOptionsWrapper'><div class='placeholder'>" + placeholder + "</div><div class='multipleSelectOptions'></div></div>");
+
+        var m = this.attr('width') !== undefined ? this.attr('width') : 10;
+        for (var i in options) {
             $('.multipleSelectOptions').append(options[i]['element']);
-            if(options[i]['textLen'] > m){
+            if (options[i]['textLen'] > m) {
                 var m = options[i]['textLen'];
             }
         }
 
         //CSS
-        $('.multipleSelectOptionsWrapper').css({'width':width});
-        $('.multipleSelectOptions').css({'width':m*8+'px'});
+        $('.multipleSelectOptionsWrapper').css({'width': width});
+        $('.multipleSelectOptions').css({'width': m * 9 + 'px'});
 
         //Action
         $(".multipleSelectOptionsWrapper .placeholder").click(function () {
             $('.multipleSelectOptions').toggle();
         });
 
-        $(this).hide().attr('disabled', 'disabled');
-
-    });
-
-});
+        this.hide().attr('disabled', 'disabled');
+    }
+}(jQuery));
